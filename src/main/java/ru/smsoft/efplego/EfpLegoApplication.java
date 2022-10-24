35d33b0ab9af1ab5c282c61a03fdd751fce5bab1
anton-lapin <lapin_antoine@mail.ru>
2022-10-24 16:54:26 +0300

    initial

---
+++ src/main/java/ru/smsoft/efplego/EfpLegoApplication.java
@@ -0,0 +1,13 @@
+package ru.smsoft.efplego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfpLegoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfpLegoApplication.class, args);
+    }
+
+}
